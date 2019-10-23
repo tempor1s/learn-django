362de089dfaa4b632a501407967b64c78807e48e
Ben <benlaugherty@gmail.com>
2019-10-22 19:51:05 -0700

    [add] detail, results, vote vies with corresponding url patterns

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking for a question at {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You are at looking for the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
