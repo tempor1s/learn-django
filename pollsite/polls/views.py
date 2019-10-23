2878819be0219ea133e87103fa0006db817727b2
Ben <benlaugherty@gmail.com>
2019-10-22 20:09:44 -0700

    [cleanup] detail view

--- pollsite/polls/views.py
+++ pollsite/polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -9,19 +9,14 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list
     }
-
+    
     return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
 
-    context = {'question': question}
-    
-    return render(request, 'polls/detail.html', context)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
