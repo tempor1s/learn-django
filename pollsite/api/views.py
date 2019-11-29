4017dc4226ba1b60deefa7b3a226f1355949f04f
Ben <benlaugherty@gmail.com>
2019-11-29 12:04:39 -0800

    [add] generic views for api

--- pollsite/api/views.py
+++ pollsite/api/views.py
@@ -1,19 +1,13 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
 
-from polls.models import Question, Choice
+from polls.models import Question
 from api.serializers import QuestionSerializer
 from api.serializers import ChoiceSerializer
 
-class QuestionList(APIView):
-    def get(self, request):
-        questions = Question.objects.all()[:20]
-        data = QuestionSerializer(questions, many=True).data
-        return Response(data)
+class QuestionList(ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
-class QuestionDetail(APIView):
-    def get(self, request, pk):
-        question = get_object_or_404(Question, pk=pk)
-        data = QuestionSerializer(question).data
-        return Response(data)
+class QuestionDetail(RetrieveDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
